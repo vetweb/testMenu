12ed175cc0bf2472f39ba7b8d1dd80d61a658070
idalgo <vetweba@gmail.com>
2020-06-12 18:44:53 +0300

    Close all by ESC

--- app/js/common.js
+++ app/js/common.js
@@ -1,69 +1,80 @@
-$(function(){
+$(function () {
+
     var subMenuItems = $('[data-menu-item-has-submenu]');
 
-    function clearMenuTimer(menuTimer){
-        if(menuTimer){
+    function clearMenuTimer(menuTimer) {
+        if (menuTimer) {
             clearTimeout(menuTimer);
             menuTimer = null;
         }
     }
 
-    function delayedOpenSubMenu(menuItem, force){
+    function delayedOpenSubMenu(menuItem, force) {
         clearMenuTimer(getItemTimer(menuItem, 'close'));
         clearMenuTimer(getItemTimer(menuItem, 'open'));
-        if(!force){
-            setItemTimer(menuItem, 'open', setTimeout(function(){
+        if (!force) {
+            setItemTimer(menuItem, 'open', setTimeout(function () {
                 openSubMenu(menuItem);
             }, 200));
-        }
-        else{
+        } else {
             openSubMenu(menuItem);
         }
     }
 
-    function delayedCloseSubMenu(menuItem, force){
+    function delayedCloseSubMenu(menuItem, force) {
         clearMenuTimer(getItemTimer(menuItem, 'close'));
         clearMenuTimer(getItemTimer(menuItem, 'open'));
-        if(!force){
-            setItemTimer(menuItem, 'close', setTimeout(function(){
+        if (!force) {
+            setItemTimer(menuItem, 'close', setTimeout(function () {
                 closeSubMenu(menuItem);
             }, 1000));
-        }
-        else{
+        } else {
             closeSubMenu(menuItem);
         }
+
+
     }
 
-    function openSubMenu(menuItem){
+    function openSubMenu(menuItem) {
         menuItem.addClass('hover');
-        subMenuItems.each(function(){
-            if(menuItem[0] !== this){
+        subMenuItems.each(function () {
+            if (menuItem[0] !== this) {
                 delayedCloseSubMenu($(this), true);
             }
         });
     }
 
-    function closeSubMenu(menuItem){
+    function closeSubMenu(menuItem) {
         menuItem.removeClass('hover');
     }
 
-    function getItemTimer(menuItem, code){
-        return menuItem.data('timer-'+code);
+    function getItemTimer(menuItem, code) {
+        return menuItem.data('timer-' + code);
     }
 
-    function setItemTimer(menuItem, code, timer){
-        return menuItem.data('timer-'+code, timer);
+    function setItemTimer(menuItem, code, timer) {
+        return menuItem.data('timer-' + code, timer);
     }
 
     subMenuItems
-        .on('mouseenter', function(){
+        .on('mouseenter', function () {
             console.log(this);
             console.log($(this));
-            //openSubMenu($(this));
             delayedOpenSubMenu($(this));
         })
-        .on('mouseleave', function(){
+        .on('mouseleave', function () {
             //closeSubMenu($(this));
             delayedCloseSubMenu($(this));
         });
+
+    function closeAll (){
+        subMenuItems.each(function () {
+            delayedCloseSubMenu($(this), true);
+        });
+    }
+    $(document).on('keydown', function (event) {
+        if (event.which === 27) {
+            closeAll();
+        }
+    });
 });
